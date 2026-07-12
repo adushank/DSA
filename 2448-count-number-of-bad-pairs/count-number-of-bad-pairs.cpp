084b2e2790472e65332df491924c55749d1513b1
ADITYA SHASHANK <144445254+adushank@users.noreply.github.com>
2026-07-13 00:32:35 +0530

    Time: 80 ms (16.79%) | Memory: 92.5 MB (35.48%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
@@ -9,13 +9,11 @@ public:
         for (int i = 0; i < n; i++) {
             long long key = (long long)nums[i] - i;
 
-            
+            good +=freq[key];
             freq[key]++;
         }
         
-        for (auto [el, cnt] : freq) {
-    good += 1LL * cnt * (cnt - 1) / 2;
-}
+       
 
         long long total = 1LL * n * (n - 1) / 2;
 
